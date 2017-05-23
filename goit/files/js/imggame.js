daec595cb347ab7c5e0408ea34f7c2655897c492
vikhol <vikholdev@gmail.com>
2017-05-23 10:34:17 +0700

    1.8

---
+++ goit/files/js/imggame.js
@@ -0,0 +1,44 @@
+/*window.onload = init;
+
+function init() {
+	var image = document.getElementById("zero");
+	image.onclick = showAnswer;
+
+
+
+};
+function showAnswer() {
+	var image = document.getElementById("zero");
+	    image.src = "../img/zero.jpg";
+	    image.setAttribute("class", "imggame2");
+	    
+};*/
+
+window.onload = init;
+
+function init() {
+	var image = document.getElementById("zero");
+	image.onclick = showAnswer;
+
+
+
+};
+
+//Усовершенстованная ф-я меняет картинку по клику "туда -> обратно"
+function showAnswer() {
+	var image = document.getElementById("zero");
+//Задает путь (src) в зависимости от уже установленного	src.
+//ВАЖНО: путь должен быть в виде адреса сайта "http://goit/files/img/zeroblur.jpg" 
+//даже если он ссылается на внутренние файлы
+	    if (image.src == "http://goit/files/img/zeroblur.jpg") {
+	    	console.log("image src: " + image.src);
+	    	image.src = "../img/zero.jpg";
+	    	image.setAttribute("class", "imggame2");
+	    } else {
+	    	console.log("image src: " + image.src);
+	    	image.src = "../img/zeroblur.jpg";
+	    	image.setAttribute("class", "imggame1");
+	    }
+		
+
+}
