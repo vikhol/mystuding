21a781b6c1a7fe3adf098a6a96a23df3c11c1a69
vikhol <vikholdev@gmail.com>
2017-05-24 22:55:15 +0700

    1.91

--- goit/files/js/imggame2.js
+++ goit/files/js/imggame2.js
@@ -1,27 +1,30 @@
 window.onload = init;
 
-function init() {
+function init() { 
 //getElementsByTagName находит все теги изображений на странице 
 //и возвращает объект типа NodeList с которым можно работать как с массивом
-	var images = document.getElementsByTagName("img");
-	console.log(images);
+    var images = document.getElementsByTagName("img");
+    console.log(images);
 //далее цикл перебирает все эл-ты	
-	for (var i = 0; i < images.length; i++) {
+    for (var i = 0; i < images.length; i++) {
 //и каждому из них назначает обработчик клика "showAnswer" 		
-		images[i].onclick = showAnswer;
-	}
+        images[i].onclick = showAnswer;
+    }
 };
 //Задаем параметр для объекта события, чтобы использовать его далее в коде.
 function showAnswer(eventObject) {
 // .target сообщает, какой элемент сгенерировал данное событие. 	
-	var image = eventObject.target;
-	console.log(image);
+    var image = eventObject.target;
+    console.log(image);
 //переменной name присваиваем название id эл-та.
-	var name = image.id;
-	console.log(name);
-//а затем добавляем к ней "расширение" .jpg	
-	name = name + ".jpg";
-//прописываем путь "src" к файлу, склеивая строки с путем до директории и полученным именем файла
-	image.src = "../img/" + name;
+    var name = image.id;
+//Усовершенстованная ф-я меняет картинку по клику "туда -> обратно"
+//ВАЖНО: путь должен быть в виде адреса сайта "http://goit/files/img/zeroblur.jpg" 
+//даже если он ссылается на внутренние файлы
+    if (image.src == "http://goit/files/img/" + name + "blur.jpg") {
+        image.src = "../img/" + name + ".jpg";
+    } else if (image.src == "http://goit/files/img/" + name + ".jpg") {
+        image.src = "../img/" + name + "blur.jpg";
+    };
 
 }
