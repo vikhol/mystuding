aa95c899123a4eb3b634dc09b56e99003cc3a8c2
vikhol <vikholdev@gmail.com>
2017-05-26 23:26:08 +0700

    2.02

--- goit/files/js/generatorfraz2.js
+++ goit/files/js/generatorfraz2.js
@@ -1,7 +1,7 @@
 function makePhrases() {
 	var otvetGeneratora = document.getElementById("otvetGeneratora");
 	var word1 = ["Жена", "Собака", "Учитель", "Столяр", "Мент", "Генерал", "Баба", "Горилла"];
-	var word2 = ["готовит", "лижет", "заполняет", "пилит", "крадет", "ебет", "манит", "ест"];
+	var word2 = ["готовит", "лижет", "заполняет", "пилит", "крадет", "нюхает", "манит", "ест"];
 	var word3 = ["борщ", "яйца", "журнал", "бревно", "вещдоки", "всех", "мужика", "банан"];
 
 	var phrase = word1[Math.floor(Math.random() * word1.length)] + " " + word2[Math.floor(Math.random() * word2.length)] + " " + word3[Math.floor(Math.random() * word3.length)] + ".";
