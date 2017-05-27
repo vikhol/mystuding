8f522de48afab4dc7503ff31625ba099139e0feb
vikhol <vikholdev@gmail.com>
2017-05-28 00:56:41 +0700

    2.03

--- goit/files/js/imggame2.js
+++ goit/files/js/imggame2.js
@@ -1,19 +1,19 @@
-window.onload = init;
+/*window.onload = init;
 
 function init() { 
 //getElementsByTagName находит все теги изображений на странице 
 //и возвращает объект типа NodeList с которым можно работать как с массивом
     var images = document.getElementsByTagName("img");
     console.log(images);
-//далее цикл перебирает все эл-ты	
+//далее цикл перебирает все эл-ты   
     for (var i = 0; i < images.length; i++) {
-//и каждому из них назначает обработчик клика "showAnswer" 		
+//и каждому из них назначает обработчик клика "showAnswer"      
         images[i].onclick = showAnswer;
     }
 };
 //Задаем параметр для объекта события, чтобы использовать его далее в коде.
 function showAnswer(eventObject) {
-// .target сообщает, какой элемент сгенерировал данное событие. 	
+// .target сообщает, какой элемент сгенерировал данное событие.     
     var image = eventObject.target;
     console.log(image);
 //переменной name присваиваем название id эл-та.
@@ -27,4 +27,25 @@ function showAnswer(eventObject) {
         image.src = "../img/" + name + "blur.jpg";
     };
 
+}*/
+
+window.onload = function() {
+    var images = document.getElementsByTagName("img");
+    for (var i = 0; i < images.length; i++) {
+        images[i].onclick = showAnswer;
+    }
+};
+
+function showAnswer(eventObj) {
+    var image = eventObj.target;
+    var name = image.id;
+    name = name + ".jpg";
+    image.src = "../img/" + name;
+//setTimeout может не только запускать ф-ии, но и передавать им аргументы (image)  
+    setTimeout(reblur, 1000, image);
+}
+function reblur(image) {
+    var name = image.id;
+    name += "blur.jpg";
+    image.src = "../img/" + name;
 }
